f6b2bb7f4c8b12558a256a0de2f5027035ab4e87
brumlablo <barbora.blozonova@gmail.com>
2015-05-03 23:08:22 +0200

    update of all

---
+++ src/ija/client/gui/LobbyPlayer.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ija.client.gui;
+
+/**
+ * Trida reprezentujici hrace v JListu pro Lobby
+ * 
+ */
+public class LobbyPlayer {
+    String name = "";
+    int ID = -1;
+
+    public LobbyPlayer(int ID) {
+        this.ID = ID;
+        name = "hrac " + ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+    
+    @Override
+    public String toString() {
+        return this.name;
+    }
+    
+}
