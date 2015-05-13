f58f167d63c54a4a767dccb54b2702c82ccb69d4
Ondrej Hajek <nixx.oh@gmail.com>
2015-05-13 20:58:46 +0200

    opravenej getlibs

--- lib/get-libs.sh
+++ lib/get-libs.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget "https://www.dropbox.com/s/a2mw0pg5j1i0l99/textures.zip"
-unzip textures.zip && rm textures.zip
+wget "https://www.dropbox.com/s/a2mw0pg5j1i0l99/textures.zip" -P lib/
+unzip lib/textures.zip  -d lib/
+rm lib/textures.zip
